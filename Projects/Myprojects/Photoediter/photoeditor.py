5ecd361a29966ffc8d6b1390d793299a40f6a3f4
01Shimbo <shane.duke88@gmail.com>
2022-10-03 13:29:00 -0600

    moving to laptop

--- Projects/Myprojects/Photoediter/photoeditor.py
+++ Projects/Myprojects/Photoediter/photoeditor.py
@@ -1,13 +1,14 @@
 from importlib.resources import path
 from PIL import Image, ImageEnhance, ImageFilter
 import os
+        ('C:','Users', 'Shane', 'OneDrive', 'Pictures', 'Saved Pictures')
+pathIn = os.path.join ('C:\\Users\\Shane\\OneDrive\\Pictures\\Saved Pictures\\Input'
+)
+os.mkdir('C:\\Users\\Shane\\OneDrive\\Pictures\\Saved Pictures\\Output')
+pathOut = 'C:\\Users\\Shane\\OneDrive\\Pictures\\Saved Pictures\\Output'
 
-path = 'C:\\Users\\Shane\\OneDrive\\Pictures\\Saved Pictures\\.LylaSecret'
-os.mkdir('C:\\Users\\Shane\\OneDrive\\Pictures\\Saved Pictures\\.editedLylaSecret')
-pathOut = 'C:\\Users\\Shane\\OneDrive\\Pictures\\Saved Pictures\\.editedLylaSecret'
-
-for filename in os.listdir(path):
-    img = Image.open(f"{path}/{filename}")
+for filename in os.listdir(pathIn):
+    img = Image.open(f"{pathIn}/{filename}")
 
     edit = img.filter(ImageFilter.SHARPEN)
     
