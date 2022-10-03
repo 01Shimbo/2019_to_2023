8ce028410fb7a5478acd67591d3cbc40d1bf4820
01Shimbo <shane.duke88@gmail.com>
2022-10-03 14:20:44 -0600

    Moving to desktop

--- Personal/Python/Os_notes.py
+++ Personal/Python/Os_notes.py
@@ -6,9 +6,18 @@ using double backslash puls works accross differnt OS
 import os 
 path = os.path.join ('C:','Users', 'Shane', 'OneDrive', 'Pictures', 'Saved Pictures')
 # isExist = os.path.exists(path)
-print(path)
+# print(path)
 newpath = os.path.join (path, 'editedLylaSecret')
-os.makedirs(newpath)
+# os.makedirs(newpath)
 newisExist = os.path.exists(newpath)
-print(newisExist)
+# print(newisExist)
+
+'''
+using os.path.expanduser('~') will get users home dir
+'''
+# usrpath = os.path.expanduser('~')
+docpath = os.path.join (os.path.expanduser('~'), 'Documents')
+docpathExist = os.path.exists(docpath)
+print (docpathExist)
+print(docpath)
 
--- Projects/Myprojects/Photoediter/photoeditor.py
+++ Projects/Myprojects/Photoediter/photoeditor.py
@@ -1,16 +1,25 @@
 from importlib.resources import path
 from PIL import Image, ImageEnhance, ImageFilter
 import os
-        ('C:','Users', 'Shane', 'OneDrive', 'Pictures', 'Saved Pictures')
-pathIn = os.path.join ('C:\\Users\\Shane\\OneDrive\\Pictures\\Saved Pictures\\Input'
-)
-os.mkdir('C:\\Users\\Shane\\OneDrive\\Pictures\\Saved Pictures\\Output')
-pathOut = 'C:\\Users\\Shane\\OneDrive\\Pictures\\Saved Pictures\\Output'
 
-for filename in os.listdir(pathIn):
-    img = Image.open(f"{pathIn}/{filename}")
+      
+pathIn  = os.path.join (os.path.expanduser('~'), 'Documents', 'pyPhotoeditor', 'Input')
+pathOut  = os.path.join (os.path.expanduser('~'), 'Documents', 'pyPhotoeditor', 'Output')
 
-    edit = img.filter(ImageFilter.SHARPEN)
+pathInExist = os.path.exists(pathIn)
+if pathInExist == False:
+    os.makedirs(pathIn)
+    print(f'New path created: {pathIn}')
+
+pathOutExist = os.path.exists(pathOut)
+if pathOutExist == False:
+    os.makedirs(pathOut)
+    print(f'New path created: {pathOut}')
+
+# for filename in os.listdir(pathIn):
+#     img = Image.open(f"{pathIn}/{filename}")
+
+#     edit = img.filter(ImageFilter.SHARPEN)
     
-    clean_name = os.path.splitext(filename)[0]
-print(os.path)
+#     clean_name = os.path.splitext(filename)[0]
+# print(os.path)
