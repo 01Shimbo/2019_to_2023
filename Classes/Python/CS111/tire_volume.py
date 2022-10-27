5e8299eb00fc4b6895d2524d556758580a5e0125
01Shimbo <shane.duke88@gmail.com>
2022-10-27 15:16:30 -0600

    pushing to class

--- Classes/Python/CS111/tire_volume.py
+++ Classes/Python/CS111/tire_volume.py
@@ -6,9 +6,9 @@ import pathlib
 curDate = datetime.now(tz=None)
 volPath = os.path.join(pathlib.Path().resolve(), 'volumes.txt')
 print(f'Current date: {curDate:%Y-%m-%d}')
-width = int (input("Enter the width of the tire in mm (ex 205): "))
-aspectRatio = int (input("Enter the aspect ratio of the tire (ex 60): "))
-diameter = int (input("Enter the diameter of the wheel in inches (ex 15):"))
+width = int(input("Enter the width of the tire in mm (ex 205): "))
+aspectRatio = int(input("Enter the aspect ratio of the tire (ex 60): "))
+diameter = int(input("Enter the diameter of the wheel in inches (ex 15):"))
 
 vol = math.pi * (width ** 2) * aspectRatio * \
     ((width * aspectRatio) + (2_540 * diameter)) / 10_000_000_000
@@ -24,8 +24,9 @@ elif width == 235 and aspectRatio == 65 and diameter == 17:
     price = 100
 else:
     price = 0
+
 if price == 0:
-    print ("Contact us for price")
+    print("Contact us for price")
 else:
     print(f'The price is ${price}/each')
 
@@ -35,19 +36,26 @@ while True:
     if favColor == "y":
         print('What is your phone number?')
         phone = input("")
-        with open(volPath, 'a') as vol_file:
-            print(f"{curDate:%Y-%m-%d}, {width}, {aspectRatio}, {diameter}, {vol:.2f}, {phone}", file = vol_file)
+        try:
+            with open(volPath, 'a') as vol_file:
+                print(
+                    f"{curDate:%Y-%m-%d}, {width}, {aspectRatio}, {diameter}, {vol:.2f}, {phone}", file=vol_file)
+        except:
+            print("Saving to volumes.txt failed due to premsissions most likely.")
+            print(
+                "make sure your current working directory is where the script is located")
         break
     if favColor == "n":
         print("Come Again!")
-        with open(volPath, 'a') as vol_file:
-            print(f"{curDate:%Y-%m-%d}, {width}, {aspectRatio}, {diameter}, {vol:.2f}", file = vol_file)
+        try:
+            with open(volPath, 'a') as vol_file:
+                print(
+                    f"{curDate:%Y-%m-%d}, {width}, {aspectRatio}, {diameter}, {vol:.2f}", file=vol_file)
+        except:
+            print("Saving to volumes.txt failed due to premsissions most likely.")
+            print(
+                "make sure your current working directory is where the script is located")
         break
     else:
         print("type a 'y' or 'n' then press enter")
         continue
-
-
-
-
-
